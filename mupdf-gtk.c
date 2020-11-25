bf0725d8b20edfc62a72a9aaad5a298abd7164c5
Yoav Marco <yoavm448@gmail.com>
2020-11-25 23:48:07 +0200

    Create client in main

--- mupdf-gtk.c
+++ mupdf-gtk.c
@@ -43,9 +43,8 @@ static void activate(GtkApplication *app, gpointer user_data) {
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Window");
   gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+  Client *c = (Client *)user_data;
 
-  Client client;
-  Client *c = &client;
   c->container = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
   g_signal_connect(G_OBJECT(c->container), "destroy",
@@ -70,7 +69,9 @@ int main(int argc, char **argv) {
   int status;
 
   app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  Client client;
+  Client *c = &client;
+  g_signal_connect(app, "activate", G_CALLBACK(activate), c);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
